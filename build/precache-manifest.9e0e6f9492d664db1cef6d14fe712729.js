728153e9e5be34a972fb4d118ab60624b244aa09
Sagar.Bhandari <sagar.bhandari@service.nsw.gov.au>
2019-03-17 16:01:20 +1100

    Adding fixtures

---
+++ build/precache-manifest.9e0e6f9492d664db1cef6d14fe712729.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bba39181d3cbf6bfbc9f",
+    "url": "/static/js/main.28561263.chunk.js"
+  },
+  {
+    "revision": "982ea1c9307e69414988",
+    "url": "/static/js/2.7a6097d4.chunk.js"
+  },
+  {
+    "revision": "bba39181d3cbf6bfbc9f",
+    "url": "/static/css/main.cc2edaa0.chunk.css"
+  },
+  {
+    "revision": "982ea1c9307e69414988",
+    "url": "/static/css/2.71612b3b.chunk.css"
+  },
+  {
+    "revision": "72a7ba877b795f701eaab41a04dd1eb7",
+    "url": "/index.html"
+  }
+];
